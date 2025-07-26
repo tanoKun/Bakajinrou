f96fbbc961a041ee7029d7a89bed046145950e14
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-26 12:34:09 +0900

    feature: パケット用リスナーの追加
    - ライフサイクルを継承させ、同一で扱えるように

---
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/listener/packet/LifecyclePacketListener.kt
@@ -0,0 +1,58 @@
+package com.github.tanokun.bakajinrou.plugin.listener.packet
+
+import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolManager
+import com.comphenix.protocol.events.ListenerPriority
+import com.comphenix.protocol.events.PacketAdapter
+import com.comphenix.protocol.events.PacketContainer
+import com.comphenix.protocol.events.PacketEvent
+import com.github.tanokun.bakajinrou.plugin.listener.LifecycleListener
+import org.bukkit.entity.Player
+import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
+
+abstract class LifecyclePacketListener internal constructor(
+    private val plugin: Plugin, private val protocolManager: ProtocolManager, listenerDsl: LifecyclePacketListener.() -> Unit
+): Listener, LifecycleListener {
+
+    private val adapters = arrayListOf<PacketAdapter>()
+
+    init {
+        listenerDsl(this)
+    }
+
+    internal fun register(
+        packet: PacketType, listenerPriority: ListenerPriority = ListenerPriority.NORMAL, callback: (PacketContainer, Player) -> Unit
+    ) {
+        val adapter = PacketAdapterObject(listenerPriority, packet, callback)
+        adapters.add(adapter)
+    }
+
+    private inner class PacketAdapterObject(
+        listenerPriority: ListenerPriority, private val packetType: PacketType, private val callback: (PacketContainer, Player) -> Unit
+    ) : PacketAdapter(plugin, listenerPriority, packetType) {
+        override fun onPacketSending(event: PacketEvent) {
+            if (event.packetType != packetType) return
+
+            callback(event.packet, event.player)
+        }
+
+        override fun onPacketReceiving(event: PacketEvent) {
+            if (event.packetType != packetType) return
+
+            callback(event.packet, event.player)
+        }
+    }
+
+    override fun registerAll() {
+        adapters.forEach { adapter ->
+            protocolManager.addPacketListener(adapter)
+        }
+    }
+
+    override fun unregisterAll() {
+        adapters.forEach { adapter ->
+            protocolManager.removePacketListener(adapter)
+        }
+    }
+}
