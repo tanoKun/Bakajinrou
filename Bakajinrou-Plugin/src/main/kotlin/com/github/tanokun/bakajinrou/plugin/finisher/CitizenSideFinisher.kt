15382f088ed03aa61f9f86b0ea59b52fd3d184ed
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-26 12:24:27 +0900

    refactor: 表示名の統一
    - 村人 -> 市民

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/finisher/CitizenSideFinisher.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/finisher/CitizenSideFinisher.kt
@@ -1,21 +1,21 @@
 package com.github.tanokun.bakajinrou.plugin.finisher
 
-import com.github.tanokun.bakajinrou.api.participant.Participant
+import com.github.tanokun.bakajinrou.api.JinrouGame
 import com.github.tanokun.bakajinrou.api.participant.position.citizen.CitizensPosition
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
 
 class CitizenSideFinisher(
-    private val participants: List<Participant>
+    private val game: JinrouGame
 ): EachSideFinisher() {
     override fun notifyFinish() {
-        participants.forEach { participant ->
+        game.participants.forEach { participant ->
             val bukkitPlayer = getBukkitPlayer(participant) ?: return@forEach
 
             showVictorySideTitle(
                 player = bukkitPlayer,
-                text = Component.text("村人の勝利")
+                text = Component.text("市民の勝利")
                     .decorate(TextDecoration.BOLD)
                     .color(TextColor.color(0x00FF00))
             )
