eeb6b9b6120b2560125d53a70fb2105486497073
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-27 16:19:19 +0900

    refactor: いらない状態の削除

--- Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/attack/AttackResult.kt
+++ Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/attack/AttackResult.kt
@@ -1,9 +1,8 @@
 package com.github.tanokun.bakajinrou.api.attack
 
-enum class AttackResult {
-    PROTECTED_BY_TOTEM,
-    PROTECTED_BY_SHIELD,
-    PROTECTED_BY_POTION_RESISTANCE,
-    SUCCESS_ATTACK,
-    INVALID_ATTACK,
+import com.github.tanokun.bakajinrou.api.method.ProtectiveMethod
+
+sealed interface AttackResult {
+    data class Protected(val by: ProtectiveMethod): AttackResult
+    data object SuccessAttack: AttackResult
 }
