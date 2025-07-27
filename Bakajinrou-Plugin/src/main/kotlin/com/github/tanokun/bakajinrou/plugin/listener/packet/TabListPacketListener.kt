51ca37e9ca3fbe797743a55789d8c4518d4f8002
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-27 22:12:11 +0900

    fix: スペクテイターの切り替え時の不安定を修正
    - 参照型であるパケットをコピーしてそれぞれのプレイヤーごとに送信

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/listener/packet/TabListPacketListener.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/listener/packet/TabListPacketListener.kt
@@ -2,10 +2,11 @@ package com.github.tanokun.bakajinrou.plugin.listener.packet
 
 import com.comphenix.protocol.PacketType
 import com.comphenix.protocol.ProtocolManager
+import com.comphenix.protocol.events.ListenerPriority
+import com.comphenix.protocol.wrappers.EnumWrappers
 import com.github.tanokun.bakajinrou.api.JinrouGame
 import com.github.tanokun.bakajinrou.plugin.formatter.display.PrefixModifier
 import com.github.tanokun.bakajinrou.plugin.formatter.display.TabListModifier
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket
 import org.bukkit.plugin.Plugin
 
 class TabListPacketListener(
@@ -16,12 +17,31 @@ class TabListPacketListener(
     val tabModifiers = jinrouGame.participants
         .associate { it.uniqueId to TabListModifier(viewer = it, prefixModifiers) }
 
-    register(packet = PacketType.Play.Server.PLAYER_INFO) { packet, receiver ->
-        val entries = packet.modifier.read(1) as MutableList<*>
+    register(packet = PacketType.Play.Server.PLAYER_INFO, listenerPriority = ListenerPriority.LOW) { event, packet, receiver ->
+        if (!packet.playerInfoActions.read(0).contains(EnumWrappers.PlayerInfoAction.UPDATE_GAME_MODE)) return@register
+        val tabModifier = tabModifiers[receiver.uniqueId] ?: return@register
 
-        val modifier = tabModifiers[receiver.uniqueId] ?: return@register
-        val modifiedList = modifier.modifyContents(entries.map { it as ClientboundPlayerInfoUpdatePacket.Entry })
+        val copy = packet.deepClone().apply {
+            playerInfoDataLists.write(1, tabModifier.modifyByUpdateGameMode(packet.playerInfoDataLists.read(1)))
+        }
 
-        packet.modifier.write(1, modifiedList)
+        event.packet = copy
+    }
+
+    register(packet = PacketType.Play.Server.PLAYER_INFO, listenerPriority = ListenerPriority.LOW) { event, packet, receiver ->
+        if (!packet.playerInfoActions.read(0).contains(EnumWrappers.PlayerInfoAction.UPDATE_DISPLAY_NAME)) return@register
+        val tabModifier = tabModifiers[receiver.uniqueId] ?: return@register
+
+        val copy = packet.deepClone().apply {
+            playerInfoDataLists.write(1, tabModifier.modifyByUpdateDisplayName(packet.playerInfoDataLists.read(1)))
+        }
+
+        event.packet = copy
+    }
+
+    register(packet = PacketType.Play.Server.PLAYER_INFO_REMOVE, listenerPriority = ListenerPriority.LOW) { event, packet, receiver ->
+        val players = packet.uuidLists.read(0).mapNotNull { jinrouGame.getParticipant(it) }
+
+        if (players.isNotEmpty()) event.isCancelled = true
     }
 })
