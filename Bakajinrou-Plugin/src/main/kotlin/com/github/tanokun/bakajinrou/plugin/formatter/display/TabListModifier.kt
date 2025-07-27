0d759e6ece2bb6696586ac0fb0810d0f67fd6939
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-27 22:14:07 +0900

    refactor: 適さない変更の削除
    - Updateごとに分離

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/formatter/display/TabListModifier.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/formatter/display/TabListModifier.kt
@@ -1,10 +1,11 @@
 package com.github.tanokun.bakajinrou.plugin.formatter.display
 
+import com.comphenix.protocol.wrappers.EnumWrappers
+import com.comphenix.protocol.wrappers.PlayerInfoData
+import com.comphenix.protocol.wrappers.WrappedChatComponent
 import com.github.tanokun.bakajinrou.api.participant.Participant
 import io.papermc.paper.adventure.PaperAdventure
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket
-import net.minecraft.world.level.GameType
-import org.bukkit.Bukkit
+import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 import plutoproject.adventurekt.component
 import plutoproject.adventurekt.text.raw
@@ -15,35 +16,51 @@ class TabListModifier(
     private val viewer: Participant,
     private val prefixModifiers: List<PrefixModifier>
 ) {
-    fun modifyContents(contents: List<ClientboundPlayerInfoUpdatePacket.Entry>): List<ClientboundPlayerInfoUpdatePacket.Entry> {
-        return contents.mapNotNull { entry ->
-            val targetPlayer = Bukkit.getPlayer(entry.profileId) ?: return@mapNotNull null
-            val prefixModifier = prefixModifiers.firstOrNull { it.target.uniqueId == entry.profileId } ?: return@mapNotNull entry
 
-            val gameType = if (isVisibleSpectator(entry.profileId)) GameType.byId(targetPlayer.gameMode.value) else GameType.SURVIVAL
+    fun modifyByUpdateGameMode(contents: List<PlayerInfoData>): List<PlayerInfoData>  {
+        return contents.map { entry ->
+            val gameType = if (isVisibleSpectator(entry.profileId)) entry.gameMode else EnumWrappers.NativeGameMode.SURVIVAL
+
+            return@map PlayerInfoData(
+                entry.profileId,
+                entry.latency,
+                entry.isListed,
+                gameType,
+                entry.profile,
+                entry.displayName,
+                entry.remoteChatSessionData,
+            )
+        }
+    }
+
+    fun modifyByUpdateDisplayName(contents: List<PlayerInfoData>): List<PlayerInfoData>  {
+        return contents.map { entry ->
+            val gameType = if (isVisibleSpectator(entry.profileId)) entry.gameMode else EnumWrappers.NativeGameMode.SURVIVAL
+            val prefixModifier = prefixModifiers.firstOrNull { it.target.uniqueId == entry.profileId } ?: return@map entry
 
             val displayName = component {
                 val prefix = prefixModifier.createPrefix(viewer = viewer)
 
-                if (prefix != net.kyori.adventure.text.Component.text("")) {
+                if (prefix != Component.text("")) {
                     raw { prefix }
                     text(" ")
                 }
 
-                text(targetPlayer.name)
+                text(entry.profile.name)
             }
 
-            return@mapNotNull ClientboundPlayerInfoUpdatePacket.Entry(
+            return@map PlayerInfoData(
                 entry.profileId,
-                entry.profile,
-                entry.listed,
                 entry.latency,
+                entry.isListed,
                 gameType,
-                PaperAdventure.asVanilla(displayName),
-                entry.chatSession
+                entry.profile,
+                WrappedChatComponent.fromHandle(PaperAdventure.asVanilla(displayName)),
+                entry.remoteChatSessionData,
             )
         }
     }
+
 
     fun isVisibleSpectator(uniqueId: UUID) = viewer.isVisibleSpectators() || uniqueId == viewer.uniqueId
 }
