9c9d8559ea625a923c869312c2a9eaeed0d01712
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-28 22:08:36 +0900

    feature: エラーハンドラの追加

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/logger/JinrouActionLogger.kt
+++
@@ -1,9 +0,0 @@
-package com.github.tanokun.bakajinrou.plugin.logger
-
-import com.github.tanokun.bakajinrou.game.logger.GameActionLogger
-import java.util.*
-
-class JinrouActionLogger: GameActionLogger {
-    override fun logKillParticipantToSpectator(victim: UUID, by: UUID) {
-    }
-}
