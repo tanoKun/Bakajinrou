481900a59fbfb56c4a748e11498df39f32fde36a
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-28 20:23:03 +0900

    feature: 偽トーテムの使用優先度を HIGH に

--- Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/protect/method/item/FakeTotemItem.kt
+++ Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/protect/method/item/FakeTotemItem.kt
@@ -6,7 +6,7 @@ import com.github.tanokun.bakajinrou.api.method.AttackMethod
 import com.github.tanokun.bakajinrou.api.method.ProtectiveMethod
 
 abstract class FakeTotemItem: ProtectiveMethod {
-    override val priority: ActivationPriority = ActivationPriority.LOW
+    override val priority: ActivationPriority = ActivationPriority.HIGH
 
     override fun verifyProtect(method: AttackMethod): AttackResult = AttackResult.SuccessAttack
 }
