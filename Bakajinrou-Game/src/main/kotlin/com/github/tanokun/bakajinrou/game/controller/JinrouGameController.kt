3b7b0d3cbe8699c9886e0928a72ae266650124a4
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-28 20:24:05 +0900

    feature: ゲーム専用の Coroutine Scope を追加
    - ゲーム上の Coroutine 操作の譲渡

--- Bakajinrou-Game/src/main/kotlin/com/github/tanokun/bakajinrou/game/controller/JinrouGameController.kt
+++ Bakajinrou-Game/src/main/kotlin/com/github/tanokun/bakajinrou/game/controller/JinrouGameController.kt
@@ -7,17 +7,31 @@ import com.github.tanokun.bakajinrou.api.participant.Participant
 import com.github.tanokun.bakajinrou.game.logger.BodyHandler
 import com.github.tanokun.bakajinrou.game.logger.GameActionLogger
 import com.github.tanokun.bakajinrou.game.scheduler.GameScheduler
+import com.github.tanokun.bakajinrou.game.scheduler.schedule.onCancellation
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlin.coroutines.CoroutineContext
 
 class JinrouGameController(
     private val game: JinrouGame,
     private val logger: GameActionLogger,
     private val scheduler: GameScheduler,
-    private val bodyHandler: BodyHandler
+    private val bodyHandler: BodyHandler,
+    defaultDispatcher: CoroutineContext
 ) {
+    private val job: Job = SupervisorJob()
+
+    val scope: CoroutineScope = CoroutineScope(job + defaultDispatcher)
+
     init {
         require(game.judge() == null) {
             "始めるにあたって、不十分な役職配布です。"
         }
+
+        scheduler.addSchedule(onCancellation {
+            job.cancel()
+        })
     }
 
     fun killed(victim: Participant, by: Participant) {
