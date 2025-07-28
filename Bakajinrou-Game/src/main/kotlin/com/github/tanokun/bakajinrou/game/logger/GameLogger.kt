fbe53db185c22898fbbf4623bb09418634e9a44c
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-28 22:03:30 +0900

    feature: 内部エラーのログを追加
    - logException(Throwable)

--- Bakajinrou-Game/src/main/kotlin/com/github/tanokun/bakajinrou/game/logger/GameLogger.kt
+++ Bakajinrou-Game/src/main/kotlin/com/github/tanokun/bakajinrou/game/logger/GameLogger.kt
@@ -2,9 +2,14 @@ package com.github.tanokun.bakajinrou.game.logger
 
 import java.util.*
 
-interface GameActionLogger {
+interface GameLogger {
     /**
      * 観戦者に、キルログを出力します。
      */
     fun logKillParticipantToSpectator(victim: UUID, by: UUID)
+
+    /**
+     * 内部的例外をコンソールに出力します
+     */
+    fun logException(throwable: Throwable)
 }
