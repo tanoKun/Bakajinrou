216de4f51af4196abcb9fc8d3bd4bb99681cfc18
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-30 15:55:34 +0900

    refactor: 責務を、UI系のログ出力のみに
    - デバッグ用のログとの分離

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/logger/JinrouLogger.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/logger/JinrouLogger.kt
@@ -1,14 +1,9 @@
 package com.github.tanokun.bakajinrou.plugin.logger
 
 import com.github.tanokun.bakajinrou.game.logger.GameLogger
-import org.bukkit.plugin.Plugin
 import java.util.*
 
-class JinrouLogger(private val plugin: Plugin): GameLogger {
+class JinrouLogger: GameLogger {
     override fun logKillParticipantToSpectator(victim: UUID, by: UUID) {
-    }
-
-    override fun logException(throwable: Throwable) {
-        plugin.logger.severe(throwable.stackTraceToString())
     }
 }
