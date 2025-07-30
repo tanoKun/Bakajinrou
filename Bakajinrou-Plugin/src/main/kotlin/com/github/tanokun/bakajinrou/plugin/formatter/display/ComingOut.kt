bf1f71f6f30e8170a57ea0d98ca724d9e934dd06
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-07-30 22:09:49 +0900

    refactor: カラーコードを統一

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/formatter/display/ComingOut.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/formatter/display/ComingOut.kt
@@ -1,11 +1,12 @@
 package com.github.tanokun.bakajinrou.plugin.formatter.display
 
+import com.github.tanokun.bakajinrou.plugin.formatter.Positions
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
 
 enum class ComingOut(val displayName: String, val color: TextColor) {
     LAST_WOLF("ラスト人狼", NamedTextColor.DARK_RED),
-    FORTUNE("占い師", TextColor.color(0x87cefa)),
-    MEDIUM("霊媒師", TextColor.color(0xff00ff)),
-    KNIGHT("騎士", TextColor.color(0x00ff7f)),
+    FORTUNE("占い師", Positions.Fortune.color),
+    MEDIUM("霊媒師", Positions.Medium.color),
+    KNIGHT("騎士", Positions.Knight.color),
 }
