8f1e2a17a613645a9fe9acd9c5cc8175f478cff7
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-03 02:23:34 +0900

    feature: 狂人に知られていることを知識化

--- Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/participant/position/wolf/WolfPosition.kt
+++ Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/participant/position/wolf/WolfPosition.kt
@@ -1,5 +1,8 @@
 package com.github.tanokun.bakajinrou.api.participant.position.wolf
 
+import com.github.tanokun.bakajinrou.api.participant.ParticipantScope
 import com.github.tanokun.bakajinrou.api.participant.position.Position
 
-interface WolfPosition: Position
+interface WolfPosition: Position {
+    fun getKnownBy(): ParticipantScope.NonSpectators
+}
