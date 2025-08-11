977aa446c4c1ac822d6909f7f6163370f44eddc5
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-11 23:29:42 +0900

    refactor: 有効判定の抽象を追加

--- Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/protect/method/ShieldMethod.kt
+++ Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/protect/method/ShieldMethod.kt
@@ -2,27 +2,33 @@ package com.github.tanokun.bakajinrou.api.protect.method
 
 import com.github.tanokun.bakajinrou.api.attack.method.ArrowMethod
 import com.github.tanokun.bakajinrou.api.attack.method.AttackMethod
-import com.github.tanokun.bakajinrou.api.method.GrantedMethod
 import com.github.tanokun.bakajinrou.api.method.MethodId
 import com.github.tanokun.bakajinrou.api.method.asMethodId
+import com.github.tanokun.bakajinrou.api.participant.ParticipantId
 import com.github.tanokun.bakajinrou.api.participant.strategy.GrantedReason
 import com.github.tanokun.bakajinrou.api.protect.ActivationPriority
 import com.github.tanokun.bakajinrou.api.protect.ProtectResult
+import com.github.tanokun.bakajinrou.api.protect.ProtectVerificator
 import com.github.tanokun.bakajinrou.api.translate.MethodAssetKeys
 import java.util.*
 
 data class ShieldMethod(
     override val methodId: MethodId = UUID.randomUUID().asMethodId(),
-    override val reason: GrantedReason
+    override val reason: GrantedReason,
+    private val verificator: ProtectVerificator
 ): ProtectiveMethod() {
     override val priority: ActivationPriority = ActivationPriority.HIGH
 
     override val assetKey: MethodAssetKeys.Protective = MethodAssetKeys.Protective.SHIELD
+
+    override val isValid: Boolean
+        get() = verificator.isValid()
 
     override fun verifyProtect(method: AttackMethod): ProtectResult = when (method) {
         is ArrowMethod -> ProtectResult.PROTECTED
         else -> ProtectResult.FAILURE
     }
 
-    override fun asTransferred(): GrantedMethod = copy(reason = GrantedReason.TRANSFERRED)
+    override fun asTransferred(participantId: ParticipantId): ProtectiveMethod =
+        copy(reason = GrantedReason.TRANSFERRED, verificator = verificator.copy(participantId = participantId))
 }
