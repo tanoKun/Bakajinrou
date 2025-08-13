0d8a10f214369154246211e0bcde145760289ffc
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-14 01:30:43 +0900

    fix: アイテムが消えるバクを修正
    - 譲渡先と譲渡元が同じ場合、差分が取れない事で追加されない

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/method/transferring/TransferMethodListener.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/method/transferring/TransferMethodListener.kt
@@ -1,12 +1,14 @@
-package com.github.tanokun.bakajinrou.plugin.interaction.method.transfer
+package com.github.tanokun.bakajinrou.plugin.interaction.method.transferring
 
 import com.github.tanokun.bakajinrou.api.JinrouGame
 import com.github.tanokun.bakajinrou.api.participant.asParticipantId
-import com.github.tanokun.bakajinrou.game.method.transfer.TransferMethod
+import com.github.tanokun.bakajinrou.game.method.transferring.TransferMethod
 import com.github.tanokun.bakajinrou.plugin.adapter.bukkit.item.ItemPersistent.getMethodId
 import com.github.tanokun.bakajinrou.plugin.adapter.bukkit.item.ItemViewer.hasPossibilityOfMethod
 import com.github.tanokun.bakajinrou.plugin.adapter.bukkit.item.ItemViewer.isTransportable
 import com.github.tanokun.bakajinrou.plugin.common.listener.LifecycleEventListener
+import com.github.tanokun.bakajinrou.plugin.common.listener.LifecycleListener
+import com.github.tanokun.bakajinrou.plugin.common.setting.builder.GameComponents
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import net.kyori.adventure.sound.Sound
@@ -15,7 +17,11 @@ import org.bukkit.NamespacedKey
 import org.bukkit.event.entity.EntityPickupItemEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.plugin.Plugin
+import org.koin.core.annotation.Scope
+import org.koin.core.annotation.Scoped
 
+@Scoped(binds = [LifecycleListener::class])
+@Scope(value = GameComponents::class)
 class TransferMethodListener(
     plugin: Plugin,
     game: JinrouGame,
@@ -46,6 +52,8 @@ class TransferMethodListener(
 
         val drop = event.item.thrower?.asParticipantId() ?: return@register
         val pickup = event.entity.uniqueId.asParticipantId()
+
+        if (drop == pickup) return@register
 
         event.isCancelled = true
         event.item.remove()
