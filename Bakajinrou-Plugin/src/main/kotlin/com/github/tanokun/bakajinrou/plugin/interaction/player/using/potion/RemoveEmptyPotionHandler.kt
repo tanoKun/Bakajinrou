699e444ae4a5954bbefd2aba0e54b9cc8c419a1d
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-14 01:35:20 +0900

    feature: 空の瓶削除の追加

---
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/player/using/potion/RemoveEmptyPotionHandler.kt
@@ -0,0 +1,20 @@
+package com.github.tanokun.bakajinrou.plugin.interaction.player.using.potion
+
+import com.github.tanokun.bakajinrou.plugin.common.listener.LifecycleEventListener
+import com.github.tanokun.bakajinrou.plugin.common.listener.LifecycleListener
+import com.github.tanokun.bakajinrou.plugin.common.setting.builder.GameComponents
+import org.bukkit.Material
+import org.bukkit.event.player.PlayerItemConsumeEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.Plugin
+import org.koin.core.annotation.Scope
+import org.koin.core.annotation.Scoped
+
+@Scoped(binds = [LifecycleListener::class])
+@Scope(value = GameComponents::class)
+class RemoveEmptyPotionHandler(plugin: Plugin): LifecycleEventListener(plugin, {
+    register<PlayerItemConsumeEvent> { event ->
+        if (event.replacement?.type != Material.GLASS_BOTTLE) return@register
+        event.replacement = ItemStack.of(Material.AIR)
+    }
+})
