0b4bb1970ab3472f29af6c3cbe53931ba3b967f8
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-14 01:20:49 +0900

    refactor: 無駄な情報の削除
    - Participant 全てを渡す必要はない

--- Bakajinrou-Game/src/main/kotlin/com/github/tanokun/bakajinrou/game/crafting/CraftingInfo.kt
+++ Bakajinrou-Game/src/main/kotlin/com/github/tanokun/bakajinrou/game/crafting/CraftingInfo.kt
@@ -1,7 +1,7 @@
 package com.github.tanokun.bakajinrou.game.crafting
 
 import com.github.tanokun.bakajinrou.api.method.GrantedMethod
-import com.github.tanokun.bakajinrou.api.participant.Participant
+import com.github.tanokun.bakajinrou.api.participant.ParticipantId
 
-data class CraftingInfo(val crafter: Participant, val style: CraftingStyle, val method: GrantedMethod) {
+data class CraftingInfo(val crafterId: ParticipantId, val style: CraftingStyle, val method: GrantedMethod) {
 }
