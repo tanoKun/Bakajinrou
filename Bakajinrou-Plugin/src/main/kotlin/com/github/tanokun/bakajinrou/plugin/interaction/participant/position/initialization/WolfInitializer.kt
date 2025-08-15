5712f196c3053185c4f104abfbc9193022c9dd32
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-15 19:22:29 +0900

    refactor: 整合性の担保できないデータの削除

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/position/initialization/WolfInitializer.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/position/initialization/WolfInitializer.kt
@@ -3,7 +3,6 @@ package com.github.tanokun.bakajinrou.plugin.interaction.participant.position.in
 import com.github.tanokun.bakajinrou.api.JinrouGame
 import com.github.tanokun.bakajinrou.api.observing.Observer
 import com.github.tanokun.bakajinrou.api.participant.ParticipantId
-import com.github.tanokun.bakajinrou.api.participant.ParticipantScope
 import com.github.tanokun.bakajinrou.api.participant.position.isWolf
 import com.github.tanokun.bakajinrou.game.participant.initialization.ParticipantInitializer
 import com.github.tanokun.bakajinrou.game.session.JinrouGameSession
@@ -22,7 +21,7 @@ class WolfInitializer(
     gameController: JinrouGameSession,
     private val playerProvider: BukkitPlayerProvider,
 ) : ParticipantInitializer(game, gameController, ::isWolf) {
-    override suspend fun initialize(selfId: ParticipantId, participants: ParticipantScope) {
+    override suspend fun initialize(selfId: ParticipantId) {
         val player = playerProvider.waitPlayerOnline(selfId)
 
         val formatter = ParticipantsFormatter(game.getCurrentParticipants().excludeSpectators(), translator)
