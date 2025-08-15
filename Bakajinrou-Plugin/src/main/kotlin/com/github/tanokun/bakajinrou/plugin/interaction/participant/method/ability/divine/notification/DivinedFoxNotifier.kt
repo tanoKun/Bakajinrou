d90515296dc768219c238fd2bfdfb82173052f34
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-15 19:21:52 +0900

    feature: 妖狐が占われた際の処理を追加

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/method/ability/divine/notification/DivinedFoxNotifier.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/method/ability/divine/notification/DivinedFoxNotifier.kt
@@ -6,15 +6,24 @@ import com.github.tanokun.bakajinrou.api.participant.position.fox.FoxPosition
 import com.github.tanokun.bakajinrou.game.ability.fortune.DivineAbilityExecutor
 import com.github.tanokun.bakajinrou.game.ability.fortune.DivineResult
 import com.github.tanokun.bakajinrou.plugin.common.bukkit.player.BukkitPlayerProvider
+import com.github.tanokun.bakajinrou.plugin.common.formatter.toTick
 import com.github.tanokun.bakajinrou.plugin.common.setting.builder.GameComponents
 import com.github.tanokun.bakajinrou.plugin.localization.JinrouTranslator
+import com.github.tanokun.bakajinrou.plugin.localization.keys.GameKeys
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.launch
+import net.kyori.adventure.sound.Sound
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.title.Title
+import org.bukkit.NamespacedKey
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import org.koin.core.annotation.Scope
 import org.koin.core.annotation.Scoped
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * 占いを監視します。占い対象が「妖狐」である場合の処理を行います。
@@ -31,6 +40,8 @@ class DivinedFoxNotifier(
     mainScope: CoroutineScope,
     executor: DivineAbilityExecutor,
 ): Observer {
+    private val glowingTime = 60.seconds
+
     init {
         mainScope.launch {
             executor
@@ -43,8 +54,13 @@ class DivinedFoxNotifier(
     }
 
     private suspend fun divined(result: DivineResult.FoundResult) {
-        val fox = playerProvider.waitPlayerOnline(result.fortuneId)
+        val fox = playerProvider.waitPlayerOnline(result.targetId)
 
-        // TODO()
+        val message = translator.translate(GameKeys.Ability.Using.DIVINED_FOX_MESSAGE, fox.locale())
+
+        fox.addPotionEffect(PotionEffect(PotionEffectType.GLOWING, glowingTime.toTick(), 1, true, false))
+        fox.showTitle(Title.title(Component.text(""), message))
+        fox.sendMessage(message)
+        fox.playSound(Sound.sound(NamespacedKey("minecraft", "block.enchantment_table.use"), Sound.Source.PLAYER, 10f, 1f))
     }
 }
