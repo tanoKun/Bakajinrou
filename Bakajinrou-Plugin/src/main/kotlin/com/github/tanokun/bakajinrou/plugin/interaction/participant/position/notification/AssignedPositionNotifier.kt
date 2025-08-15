b803ee97bbca9988731085d4e6b623a85d477eaf
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-15 19:22:49 +0900

    feature: 試合開始時の役職通知の追加

---
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/position/notification/AssignedPositionNotifier.kt
@@ -0,0 +1,34 @@
+package com.github.tanokun.bakajinrou.plugin.interaction.participant.position.notification
+
+
+import com.github.tanokun.bakajinrou.api.JinrouGame
+import com.github.tanokun.bakajinrou.api.observing.Observer
+import com.github.tanokun.bakajinrou.api.participant.ParticipantId
+import com.github.tanokun.bakajinrou.game.participant.initialization.ParticipantInitializer
+import com.github.tanokun.bakajinrou.game.session.JinrouGameSession
+import com.github.tanokun.bakajinrou.plugin.common.bukkit.player.BukkitPlayerProvider
+import com.github.tanokun.bakajinrou.plugin.common.setting.builder.GameComponents
+import com.github.tanokun.bakajinrou.plugin.localization.JinrouTranslator
+import com.github.tanokun.bakajinrou.plugin.localization.keys.GameKeys
+import org.koin.core.annotation.Scope
+import org.koin.core.annotation.Scoped
+
+@Scoped(binds = [Observer::class])
+@Scope(value = GameComponents::class)
+class AssignedPositionNotifier(
+    private val game: JinrouGame,
+    private val translator: JinrouTranslator,
+    gameController: JinrouGameSession,
+    private val playerProvider: BukkitPlayerProvider,
+) : ParticipantInitializer(game, gameController, { true }) {
+    override suspend fun initialize(selfId: ParticipantId) {
+        val self = game.getParticipant(selfId) ?: return
+        val player = playerProvider.waitPlayerOnline(selfId)
+
+        val prefix = self.getPrefix(self) ?: return
+        val positionName = translator.translate(prefix, player.locale())
+        val message = translator.translate(GameKeys.Start.Notification.POSITION, player.locale(), positionName)
+
+        player.sendMessage(message)
+    }
+}
