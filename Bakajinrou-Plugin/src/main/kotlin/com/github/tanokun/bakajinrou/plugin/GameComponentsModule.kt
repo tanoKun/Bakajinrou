db3abee014f7388ba6b4fa649b4ad31438752f6f
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-15 19:25:07 +0900

    refactor: 不要な情報の削除

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/GameComponentsModule.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/GameComponentsModule.kt
@@ -1,11 +1,8 @@
-package com.github.tanokun.bakajinrou.plugin.module
+package com.github.tanokun.bakajinrou.plugin
 
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 
 @Module
 @ComponentScan(value = ["com.github.tanokun.bakajinrou"])
-object GameComponentsModule {
-
-
-}
+object GameComponentsModule
