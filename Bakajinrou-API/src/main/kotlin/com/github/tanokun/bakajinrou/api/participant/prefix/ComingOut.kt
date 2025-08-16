c65d416af8eaf9318e31cac89278ea9fb0605a5f
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-16 21:12:02 +0900

    fix: アクセス権限の変更

--- Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/participant/prefix/ComingOut.kt
+++ Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/participant/prefix/ComingOut.kt
@@ -3,7 +3,7 @@ package com.github.tanokun.bakajinrou.api.participant.prefix
 import com.github.tanokun.bakajinrou.api.participant.Participant
 import com.github.tanokun.bakajinrou.api.translation.PrefixKeys
 
-enum class ComingOut(private val translationKey: PrefixKeys.ComingOut): PrefixSource {
+enum class ComingOut(val translationKey: PrefixKeys.ComingOut): PrefixSource {
     LAST_WOLF(PrefixKeys.ComingOut.LAST_WOLF),
     FORTUNE(PrefixKeys.ComingOut.FORTUNE),
     MEDIUM(PrefixKeys.ComingOut.MEDIUM),
