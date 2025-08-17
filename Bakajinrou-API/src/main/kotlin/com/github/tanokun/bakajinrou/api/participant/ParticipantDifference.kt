dd935c5e2d648a3c45327b53c4b3efa805a20b38
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-17 22:49:46 +0900

    feature: 参加者用、前後での変化フィルターの追加

--- Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/participant/ParticipantDifference.kt
+++ Bakajinrou-API/src/main/kotlin/com/github/tanokun/bakajinrou/api/participant/ParticipantDifference.kt
@@ -1,3 +1,13 @@
 package com.github.tanokun.bakajinrou.api.participant
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNot
+
 data class ParticipantDifference(val before: Participant?, val after: Participant)
+
+fun <T> Flow<ParticipantDifference>.distinctUntilChangedByParticipantOf(keySelector: (Participant) -> T) = this.filterNot {
+    val before = it.before?.let { beforeParticipant -> keySelector(beforeParticipant) }
+    val after = keySelector(it.after)
+
+    before == after
+}
