d100f2e14027d27552e51dc507ffff91fd3f1f09
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-17 22:51:40 +0900

    fix: 変更と無変更のフィルターがうまくいっていないのを修正

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/dead/DeathConfirmedObserver.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/dead/DeathConfirmedObserver.kt
@@ -3,13 +3,12 @@ package com.github.tanokun.bakajinrou.plugin.interaction.participant.dead
 import com.github.tanokun.bakajinrou.api.JinrouGame
 import com.github.tanokun.bakajinrou.api.observing.Observer
 import com.github.tanokun.bakajinrou.api.participant.Participant
+import com.github.tanokun.bakajinrou.api.participant.distinctUntilChangedByParticipantOf
 import com.github.tanokun.bakajinrou.plugin.common.bukkit.player.BukkitPlayerProvider
 import com.github.tanokun.bakajinrou.plugin.common.setting.builder.GameComponents
 import com.github.tanokun.bakajinrou.plugin.interaction.participant.dead.body.BukkitBodyHandler
 import com.github.tanokun.bakajinrou.plugin.interaction.participant.dead.body.DisableHittingBody
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import org.bukkit.GameMode
@@ -29,8 +28,7 @@ class DeathConfirmedObserver(
     init {
         mainScope.launch {
             game.observeParticipants(mainScope)
-                .filter { it.after.isDead() }
-                .distinctUntilChanged()
+                .distinctUntilChangedByParticipantOf(Participant::isDead)
                 .map { it.after }
                 .collect(::onDeath)
         }
