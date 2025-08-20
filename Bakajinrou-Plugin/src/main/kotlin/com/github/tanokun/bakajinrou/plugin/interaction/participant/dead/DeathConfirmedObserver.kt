540f75ee8bf169f2070831e62136817423265ebb
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-20 19:23:57 +0900

    refactor: 死亡時の共通処理から、死体関係を分離

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/dead/DeathConfirmedObserver.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/dead/DeathConfirmedObserver.kt
@@ -6,13 +6,10 @@ import com.github.tanokun.bakajinrou.api.participant.Participant
 import com.github.tanokun.bakajinrou.api.participant.distinctUntilChangedByParticipantOf
 import com.github.tanokun.bakajinrou.plugin.common.bukkit.player.BukkitPlayerProvider
 import com.github.tanokun.bakajinrou.plugin.common.setting.builder.GameComponents
-import com.github.tanokun.bakajinrou.plugin.interaction.participant.dead.body.BukkitBodyHandler
-import com.github.tanokun.bakajinrou.plugin.interaction.participant.dead.body.DisableHittingBody
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import org.bukkit.GameMode
-import org.bukkit.craftbukkit.entity.CraftPlayer
 import org.koin.core.annotation.Scope
 import org.koin.core.annotation.Scoped
 
@@ -20,10 +17,8 @@ import org.koin.core.annotation.Scoped
 @Scope(value = GameComponents::class)
 class DeathConfirmedObserver(
     private val game: JinrouGame,
-    private val bodyHandler: BukkitBodyHandler,
     private val playerProvider: BukkitPlayerProvider,
     private val mainScope: CoroutineScope,
-    private val disableHittingBody: DisableHittingBody,
 ): Observer {
     init {
         mainScope.launch {
@@ -38,8 +33,6 @@ class DeathConfirmedObserver(
         val player = playerProvider.getAllowNull(dead) ?: return
 
         player.inventory.clear()
-        disableHittingBody.ghost(player as CraftPlayer)
-        bodyHandler.createBody(dead.participantId)
         player.gameMode = GameMode.SPECTATOR
     }
 }
