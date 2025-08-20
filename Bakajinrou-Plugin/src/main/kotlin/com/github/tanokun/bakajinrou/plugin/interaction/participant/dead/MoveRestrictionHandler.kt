160a0f36719b31d5941551241ab65ce6cb8b1227
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-08-20 19:24:36 +0900

    fix: スペクテイターには見えるように

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/dead/MoveRestrictionHandler.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/participant/dead/MoveRestrictionHandler.kt
@@ -6,7 +6,8 @@ import com.comphenix.protocol.events.ListenerPriority
 import com.github.tanokun.bakajinrou.plugin.common.listener.LifecycleListener
 import com.github.tanokun.bakajinrou.plugin.common.listener.packet.LifecyclePacketListener
 import com.github.tanokun.bakajinrou.plugin.common.setting.builder.GameComponents
-import com.github.tanokun.bakajinrou.plugin.interaction.participant.dead.body.DisableHittingBody
+import com.github.tanokun.bakajinrou.plugin.interaction.participant.dead.body.restriction.DisableHittingBody
+import org.bukkit.GameMode
 import org.bukkit.plugin.Plugin
 import org.koin.core.annotation.Scope
 import org.koin.core.annotation.Scoped
@@ -16,7 +17,7 @@ import org.koin.core.annotation.Scoped
 class MoveRestrictionHandler(
     plugin: Plugin,
     disableHittingBody: DisableHittingBody,
-    protocolManager: ProtocolManager,
+    protocolManager: ProtocolManager
 ) : LifecyclePacketListener(plugin, protocolManager, {
     register(
         packet = arrayOf(
@@ -25,6 +26,7 @@ class MoveRestrictionHandler(
             PacketType.Play.Server.ENTITY_LOOK),
         listenerPriority = ListenerPriority.LOW
     ) { event, packet, receiver ->
-        if (disableHittingBody.ghostingPlayers.contains(packet.integers.read(0))) event.isCancelled = true
+        if (!disableHittingBody.ghostingPlayers.contains(packet.integers.read(0))) return@register
+        if (receiver.gameMode != GameMode.SPECTATOR) event.isCancelled = true
     }
 })
