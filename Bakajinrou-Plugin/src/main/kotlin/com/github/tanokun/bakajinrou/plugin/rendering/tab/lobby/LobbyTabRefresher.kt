0d95ac23c6c52cf36960757680cfe39bed351b2b
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-10-27 20:11:39 +0900

    feature: ロビー内のプレイヤーのタブ情報を更新する処理の実装
    ロビーに参加したプレイヤーのタブ情報をリアルタイムで更新するための処理を実装した
    - プレイヤーの参加、退出、ゲームモード変更に応じてタブ情報を更新
    - スペクテイターと候補者の情報を管理し、適切なコンポーネントを表示

---
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/rendering/tab/lobby/LobbyTabRefresher.kt
@@ -0,0 +1,98 @@
+package com.github.tanokun.bakajinrou.plugin.rendering.tab.lobby
+
+import com.github.tanokun.bakajinrou.api.participant.ParticipantId
+import com.github.tanokun.bakajinrou.plugin.common.bukkit.player.BukkitPlayerProvider
+import com.github.tanokun.bakajinrou.plugin.common.setting.GameSettings
+import com.github.tanokun.bakajinrou.plugin.localization.JinrouTranslator
+import com.github.tanokun.bakajinrou.plugin.rendering.tab.DummyUUID
+import com.github.tanokun.bakajinrou.plugin.rendering.tab.TabEntryComponent
+import com.github.tanokun.bakajinrou.plugin.rendering.tab.handler.HandlerType
+import com.github.tanokun.bakajinrou.plugin.rendering.tab.handler.TabHandler
+import com.github.tanokun.bakajinrou.plugin.rendering.tab.lobby.component.CandidateInLobbyComponent
+import com.github.tanokun.bakajinrou.plugin.rendering.tab.lobby.component.SpectatorInLobbyComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import net.minecraft.world.level.GameType
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerGameModeChangeEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import java.util.*
+
+class LobbyTabRefresher(
+    private val gameSettings: GameSettings,
+    private val tabHandler: TabHandler,
+    private val jinrouTranslator: JinrouTranslator,
+    private val playerProvider: BukkitPlayerProvider,
+    coroutineScope: CoroutineScope,
+): Listener {
+    private val playerAttaches = HashMap<UUID, DummyUUID>()
+
+    init {
+        coroutineScope.launch {
+            gameSettings.changedSpectators.collect {
+                val dummyId = getDummyUuid(it.uuid)
+                val targetPlayer = playerProvider.getAllowNull(ParticipantId(it.uuid)) ?: return@collect
+                val gameType = GameType.byId(targetPlayer.gameMode.value)
+
+                when (it) {
+                    is GameSettings.ChangedSpectator.Added -> tabHandler.editEngine(HandlerType.LOBBY) {
+                        val component = SpectatorInLobbyComponent(dummyId, targetPlayer, gameType, jinrouTranslator)
+                        updateComponent(component)
+                    }
+
+                    is GameSettings.ChangedSpectator.Removed -> tabHandler.editEngine(HandlerType.LOBBY) {
+                        val component = CandidateInLobbyComponent(dummyId, targetPlayer, gameType)
+                        updateComponent(component)
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    suspend fun onJoin(e: PlayerJoinEvent) {
+        delay(50)
+
+        tabHandler.joinEngine(HandlerType.LOBBY, e.player)
+        tabHandler.editEngine(HandlerType.LOBBY) {
+            val gameType = GameType.byId(e.player.gameMode.value)
+            addComponent(getComponent(e.player, gameType))
+        }
+    }
+
+    @EventHandler
+    fun onQuit(e: PlayerQuitEvent) {
+        tabHandler.quitEngine(e.player)
+
+        val dummyId = getDummyUuid(e.player.uniqueId)
+
+        tabHandler.editEngine(HandlerType.LOBBY) {
+            removeComponent(dummyId)
+        }
+    }
+
+    @EventHandler
+    fun onChangeGameMode(e: PlayerGameModeChangeEvent) {
+        tabHandler.editEngine(HandlerType.LOBBY) {
+            updateComponent(getComponent(e.player, GameType.byId(e.newGameMode.value)))
+        }
+    }
+
+    private fun getDummyUuid(playerUuid: UUID): DummyUUID {
+        return playerAttaches.computeIfAbsent(playerUuid) { DummyUUID.random() }
+    }
+
+    private fun getComponent(player: Player, gameType: GameType): TabEntryComponent {
+        val dummyId = getDummyUuid(player.uniqueId)
+
+        return if (player.uniqueId in gameSettings.spectators)
+             SpectatorInLobbyComponent(dummyId, player, gameType, jinrouTranslator)
+         else
+             CandidateInLobbyComponent(dummyId, player, gameType)
+
+    }
+}
