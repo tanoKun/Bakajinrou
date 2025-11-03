5bf7e322bbc3dbf9471e63da283768d54db0dbfc
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-11-03 21:49:30 +0900

    feature: マップ選択GUIの実装

---
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/player/setting/prepare/DecidedSelections.kt
@@ -0,0 +1,5 @@
+package com.github.tanokun.bakajinrou.plugin.interaction.player.setting.prepare
+
+import com.github.tanokun.bakajinrou.plugin.map.GameMap
+
+data class SelectedMap(val map: GameMap)
---
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/player/setting/prepare/RecentSelectedMap.kt
@@ -0,0 +1,6 @@
+package com.github.tanokun.bakajinrou.plugin.interaction.player.setting.prepare
+
+import com.github.tanokun.bakajinrou.plugin.map.GameMap
+
+data class RecentSelectedMap(val recentMap: GameMap) {
+}
