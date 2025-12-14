bbb479dba9978972ec50ab316d81cc64cc0c639f
tanoKun <76431283+tanokun@users.noreply.github.com>
2025-12-14 16:49:40 +0900

    feature: 参加者選択にするために、レガシーの削除

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/player/setting/prepare/DecidedSelections.kt
+++
@@ -1,5 +0,0 @@
-package com.github.tanokun.bakajinrou.plugin.interaction.player.setting.prepare
-
-import com.github.tanokun.bakajinrou.plugin.map.GameMap
-
-data class SelectedMap(val map: GameMap)
