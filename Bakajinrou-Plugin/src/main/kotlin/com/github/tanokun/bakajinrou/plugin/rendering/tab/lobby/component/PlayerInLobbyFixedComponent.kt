b3deeea4fa937f1d892e2fa61bbf62f35333d8bf
tanoKun <76431283+tanokun@users.noreply.github.com>
2026-03-13 18:22:59 +0900

    feature: 命名の変更

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/rendering/tab/lobby/component/PlayerInLobbyFixedComponent.kt
+++ Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/rendering/tab/lobby/component/PlayerInLobbyFixedComponent.kt
@@ -7,8 +7,12 @@ import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket
 import net.minecraft.world.level.GameType
 import org.bukkit.craftbukkit.entity.CraftPlayer
 import org.bukkit.entity.Player
+import plutoproject.adventurekt.component
+import plutoproject.adventurekt.text.color
+import plutoproject.adventurekt.text.style.gray
+import plutoproject.adventurekt.text.text
 
-class CandidateInLobbyFixedComponent(
+class PlayerInLobbyFixedComponent(
     override val dummyUuid: DummyUUID,
     target: Player,
     gameType: GameType
@@ -18,8 +22,12 @@ class CandidateInLobbyFixedComponent(
 
         val latency = target.ping
 
+        val displayName = component {
+            text(target.name) color gray
+        }
+
         ClientboundPlayerInfoUpdatePacket.Entry(
-            dummyUuid.uuid, gameProfile, true, latency, gameType, PaperAdventure.asVanilla(target.displayName()), true, 0, null
+            dummyUuid.uuid, gameProfile, true, latency, gameType, PaperAdventure.asVanilla(displayName), true, 0, null
         )
     }
 
