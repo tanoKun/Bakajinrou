28400461d9eeda82e38d4071e9de8d4dff4fcb3a
tanoKun <76431283+tanokun@users.noreply.github.com>
2026-03-13 18:20:04 +0900

    fix: 階層展開に伴う削除

--- Bakajinrou-Plugin/src/main/kotlin/com/github/tanokun/bakajinrou/plugin/interaction/player/setting/prepare/RecentSelectedMap.kt
+++
@@ -1,6 +0,0 @@
-package com.github.tanokun.bakajinrou.plugin.interaction.player.setting.prepare
-
-import com.github.tanokun.bakajinrou.plugin.map.GameMap
-
-data class RecentSelectedMap(val recentMap: GameMap) {
-}
